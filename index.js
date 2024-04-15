155b90170a9fa5500edb3d4c42fccc075880e657
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2024-04-15 19:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,53 @@
 $("#btn").click(() => {
-  let inputDay = $("#date").val();
-  let inputMonth = $("#month").val();
-  let inputYear = $("#year").val();
-  let y;
-  let m;
-  let d;
-  const day = new Date();
-  let currentDay = day.getDate();
-  let currentMonth = day.getMonth() + 1;
-  let currentYear = day.getFullYear();
+  let ageInYears; let ageInMonths; let ageInDays; 
   
-  if (inputDay != '' && inputMonth != '' && inputYear != '') {
-    if (inputYear <= currentYear) {
-      age(inputYear, inputMonth, inputDay);
-    }
-    else {
-      alert("Year Must be Smaller than Current Year")
-    }
-  } else {
-    alert("Please fill the required fields")
-  }
+  const d = new Date();
+  const currentYear = d.getFullYear();
+  const currentMonth = d.getMonth() + 1;
+  const currentDate = d.getDate();
+  
+  let inputYear = parseInt($("#year").val());let inputMonth = parseInt($("#month").val());let inputDate = parseInt($("#date").val());
+  
+  const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const totalDays = months[inputMonth - 1] - inputDate + currentDate;
+/*
+console.log('Total Days: ', totalDays); calculateYears(); calculateMonthsDays();
+console.log(inputYear, inputMonth, inputDate);
+console.log(currentYear, currentMonth, currentDate);
+console.log(ageInYears, ageInMonths, ageInDays);
+*/ 
 
-  $(".yearsOld").text(y);
-  $(".monthsOld").text(m);
-  $(".daysOld").text(d);
-
-  function age(year, month, day) {
-    console.log(inputDay, inputMonth, inputYear);
-    console.log(currentDay, currentMonth, currentYear);
-    if (currentMonth == inputMonth) {
-      if (currentDay == inputDay) {
-        console.log("cm=im: Year => ", y = currentYear - inputYear);
-        console.log("cd=id: Month => ", m = currentMonth - inputMonth);
-        console.log("cd=id: Day => ", d = currentDay - inputDay);
-      } else if (currentDay > inputDay) {
-        console.log("cm=im: Year => ", y = currentYear - inputYear);
-        console.log("cd > id: Month => ", m = currentMonth - inputMonth);
-        console.log("cd > id: Day => ", d =currentDay - inputDay);
-      } else {
-        console.log("cm=im: Year => ", y =  currentYear - inputYear - 1);
-        console.log("cd<id: Month => ",  m = 12 - 1);
-        console.log("cd<id: Day => ", d = 31 - inputDay + currentDay);
-      }
-    } else if (currentMonth > inputMonth) {
-      if (currentDay == inputDay) {
-        console.log("cm>im: Year => ", y = currentYear - inputYear);
-        console.log("cd=id: Month => ", m = currentMonth - inputMonth);
-        console.log("cd=id: Day => ", d = currentDay - inputDay);
-      } else if (currentDay > inputDay) {
-        console.log("cm>im: Year => ", y = currentYear - inputYear);
-        console.log("cd > id: Month => ", m = currentMonth - inputMonth);
-        console.log("cd > id: Day => ", d = currentDay - inputDay);
-      } else {
-        console.log("cm>im: Year => ", y = currentYear - inputYear);
-        console.log("cd<id: Month => ", m = currentMonth - inputMonth - 1);
-        console.log("cd<id: Day => ", d = 31 - inputDay + currentDay);
-      }
-    }else {
-      if (currentDay == inputDay) {
-        console.log("cm<im: Year => ", y =currentYear - inputYear - 1);
-        console.log("cd=id: Month => ", m = inputMonth - currentMonth);
-        console.log("cd=id: Day => ", d = currentDay - inputDay);
-      } else if (currentDay > inputDay) {
-        console.log("cm<im: Year => ", y = currentYear - inputYear - 1);
-        console.log("cd > id: Month => ",m = 12 -  inputMonth + currentMonth);
-        console.log("cd > id: Day => ", d = currentDay - inputDay);
-      } else {
-        console.log("cm<im: Year => ", y = currentYear - inputYear - 1);
-        console.log("cd<id: Month => ", m = 12 + currentMonth - inputMonth - 1);
-        console.log("cd<id: Day => ", d = 31 - inputDay + currentDay);
-      }
-    } 
+  calculateYears(); 
+  calculateMonthsDays();
+  if (inputDate >= 0 && inputYear >= 0 && inputMonth >= 0) {
+    $('.yearsOld').text(ageInYears);
+    $(".monthsOld").text(ageInMonths);
+    $(".daysOld").text(ageInDays);  
+  } else { alert("Fill the Required Fields"); }
+  
+  function calculateMonthsDays () {
+    if (currentMonth == inputMonth && currentDate >= inputDate) {
+      ageInMonths =  currentMonth - inputMonth ; ageInDays = currentDate - inputDate;       
+    }
+    else if (currentMonth == inputMonth && currentDate < inputDate) {
+      ageInMonths =  12 - 1 ; ageInDays = totalDays + 1;       
+    }
+    else if (currentMonth < inputMonth && currentDate >= inputDate) {
+      ageInMonths = 12 - inputMonth + currentMonth ; ageInDays = currentDate - inputDate;       
+    }
+    else if (currentMonth < inputMonth && currentDate < inputDate) {
+      ageInMonths =  12 - inputMonth + currentMonth - 1 ; ageInDays = totalDays + 1;       
+    }
+    else if (currentMonth > inputMonth && currentDate >= inputDate) {
+      ageInMonths =  currentMonth - inputMonth; ageInDays = currentDate - inputDate;       
+    }
+    else if (currentMonth > inputMonth && currentDate < inputDate) {
+      ageInMonths =  currentMonth - inputMonth - 1; ageInDays = totalDays;       
+    }
   }
   
+  function calculateYears() {
+    if (currentMonth >= inputMonth) { ageInYears = currentYear - inputYear; } 
+    else { ageInYears = currentYear - inputYear - 1; } 
+  }
 });
